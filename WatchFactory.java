792d2acf75280e8e590d5557bcebd06ef8992b0c
Lakshmi88028 <89910790+Lakshmi88028@users.noreply.github.com>
2021-09-07 11:10:22 +0530

    Add files via upload

---
+++ WatchFactory.java
@@ -0,0 +1,15 @@
+class WatchFactory
+{
+public static void main(String a[]){
+
+String[] watchName={"Patek Philippe","Vacheron Constantin","Audemars Piguet","Blancpain"};
+long[] priceForEach={3000,4000,5000,6000};
+
+for(int i=0;i<watchName.length;i++){
+	System.out.println(watchName[i]+"");
+}
+for(int j=0;j<priceForEach.length;j++){
+	System.out.println(priceForEach[j]+"");
+}
+}
+}
