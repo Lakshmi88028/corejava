7b877b1ddb9709db2b926f48c315ec0edd0cfe05
Lakshmi88028 <89910790+Lakshmi88028@users.noreply.github.com>
2021-09-17 19:37:55 +0530

    Add files via upload

---
+++ SpeakerUtil.java
@@ -0,0 +1,18 @@
+class SpeakerUtil{
+
+public static void main(String a[]){
+boolean isConnected=Speaker.onOrOff();
+
+System.out.println("the speaker is connected" );
+Speaker.increaseVolume();
+Speaker.increaseVolume();
+Speaker.increaseVolume();
+Speaker.increaseVolume();
+Speaker.decreaseVolume();
+Speaker.decreaseVolume();
+Speaker.decreaseVolume();
+Speaker.decreaseVolume();
+}
+
+
+}
