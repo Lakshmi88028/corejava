660a16caecdf5b29a940b28c94e6a2de8d375f0f
Lakshmi88028 <89910790+Lakshmi88028@users.noreply.github.com>
2021-12-08 00:02:58 +0530

    Update GYMCreator.java

--- com/xworkz/dao/gym/GYMCreator.java
+++ com/xworkz/dao/gym/GYMCreator.java
@@ -4,19 +4,19 @@ public class GYMCreator{
 
 public static void main(String[] args) {
 	
-	String gymName="Quadz Fitness";
+	String gymName="Akshara Gym";
 	gymName=gymName.replace("","");
 	System.out.println(gymName);
 	
 	GYMDAO dao=new GYMDAO();
-	dao.save("Quadz Fitness");
-	dao.save("JJ Gym");
-	dao.save("Moveza Fitness");
+	dao.save("Akshara Gym");
+	dao.save("Dboss Gym");
+	dao.save("Visual Fitness");
 	
-	boolean found=dao.find("Mind and Body Fitness");
+	boolean found=dao.find("Fit and Fine Fitness");
 	System.out.println(found);
 	
-	found=dao.findByCaseInsensitive("jj gym");
+	found=dao.findByCaseInsensitive("jk gym");
 	System.out.println(found);
 	
 	found=dao.findByCaseInsensitiveAndTrimmed("movezafitness");
